7c3052e4acfe0ad519220f57033ffb74a7b304e7
mariusmuresan18 <73294343+mariusmuresan18@users.noreply.github.com>
2020-11-27 15:08:18 +0200

    Add files via upload

---
+++ slideShow.js
@@ -0,0 +1,16 @@
+var slideIndex=0;
+slideshow();
+
+function slideshow(){
+	var slides = document.getElementsByClassName("mySlides");
+	
+	for(i=0;i<slides.length;i++){
+		slides[i].style.display="none";
+	}
+	slideIndex++;
+	if(slideIndex>slides.length) {slideIndex = 1}
+	
+	slides[slideIndex-1].style.display="block";
+	
+	setTimeout(slideshow, 3000); //Change image every 3 seconds
+}
